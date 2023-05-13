d8ed9f1cd12aadb4f46adf56d2612408f1369c95
ninehcobra <ttbexinhtt2903@gmail.com>
2023-05-13 09:26:28 +0700

    province db

--- src/migrations/migration-create-province.js
+++ src/migrations/migration-create-province.js
@@ -1,20 +1,14 @@
 'use strict';
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.createTable('allcodes', {
+        await queryInterface.createTable('provinces', {
             id: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
                 type: Sequelize.INTEGER
-            },
-            key: {
-                type: Sequelize.STRING
-            },
-            type: {
-                type: Sequelize.STRING
-            },
-            value: {
+            },   
+            name: {
                 type: Sequelize.STRING
             },
             createdAt: {
@@ -28,6 +22,6 @@ module.exports = {
         });
     },
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.dropTable('allcode');
+        await queryInterface.dropTable('provinces');
     }
 };
